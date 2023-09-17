71d9bbf9c8a0dbf6ded47e6fa4f9ea93c3290377
Aden O'Brien <obrad528@student.otago.ac.nz>
2023-09-17 02:14:31 -0600

    removed return from BST add

--- A3/BST.c
+++ A3/BST.c
@@ -1,90 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct
+{
 
-typedef struct{
-
-    //void * parent;
-    struct node * left_child;
-    struct node * right_child;
+    // void * parent;
+    struct node *left_child;
+    struct node *right_child;
     int value;
 
-}node;
+} node;
 
-typedef struct{
+
+
+
+typedef struct
+{
 
     int size;
-    node * root;
-    
-}BST;
+    node *root;
 
-node * new_node(int value){
-    node * output;
-    output = calloc(1,sizeof(node));
-    output -> value = value;
+} BST;
+
+
+
+
+node *new_node(int value)
+{
+    node *output;
+    output = calloc(1, sizeof(node));
+    output->value = value;
     output->left_child = NULL;
-    output->right_child = NULL; 
+    output->right_child = NULL;
     return output;
 }
 
-node * insert_node(node * root, int value){
-    if(root==NULL){
+
+
+
+node *insert_node(node *root, int value)
+{
+    if (root == NULL)
+    {
         root = new_node(value);
     }
-    else if(value <root->value){
-       root->left_child = insert_node(root -> left_child,value);
+    else if (value < root->value)
+    {
+        root->left_child = insert_node(root->left_child, value);
     }
-    else if(value >root -> value){
-        root->right_child = insert_node(root -> right_child,value);
+    else if (value > root->value)
+    {
+        root->right_child = insert_node(root->right_child, value);
     }
-    else{
+    else
+    {
         printf("Error");
     }
     return root;
 }
 
 
-BST t_add(BST input, int value){
-    if(input.root==NULL){
-        input.root = new_node(value);
-        input.size +=1;
+
+
+void add(BST *input, int value)
+{
+    if (input->root == NULL)
+    {
+        input->root = new_node(value);
+        input->size += 1;
     }
-    else if(value < input.root->value){
-       input.root->left_child = insert_node(input.root->left_child,value);
-       input.size +=1;
+    else if (value < input->root->value)
+    {
+        input->root->left_child = insert_node(input->root->left_child, value);
+        input->size += 1;
     }
-    else if(value > input.root -> value){
-       input.root->right_child = insert_node(input.root->right_child,value);
-       input.size +=1;
+    else if (value > input->root->value)
+    {
+        input->root->right_child = insert_node(input->root->right_child, value);
+        input->size += 1;
     }
-    else{
+    else
+    {
         printf("Error");
     }
-
-    return input;
-}
-
-int main(){
-
-    BST test;
-    test.root = NULL;
-    test.size =0;
-    
-    test = t_add(test,5);
-    test = t_add(test,6);
-
-    node * output = test.root->right_child;
-    int value_out = output->value;
-
-
-    printf("Value Out: %d\n",value_out);
-    printf("BST size: %d\n",test.size);
-
-return 0;
 }
 
 
 
 
+int main()
+{
 
+    BST *test;
+    test->root = NULL;
+    test->size = 0;
 
+    add(test, 5);
+    add(test, 6);
+    add(test, 7);
+
+    node *output = test->root->right_child;
+    node *output2 = output->right_child;
+    int value_out = output2->value;
+
+    printf("Value Out: %d\n", value_out);
+    printf("BST size: %d\n", test->size);
+
+    return 0;
+}
